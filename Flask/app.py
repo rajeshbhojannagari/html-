12afaec9da4da75c910705a96caa570b789dfd8c
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-02-04 12:54:07 +0530

    practising flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask,redirect,url_for,session,Response,request
 
 app=Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Welcome to the Flask app"
-# if __name__=="__main__":
-#     app.run()
+app.secret_key="supersecretkey"
+@app.route('/', methods=['POST','GET'])
+def login():
+    if request.method=='POST':
+        username=request.form.get('username')
+        password=request.form.get('password')
+        if username=='admin' and password=='123':
+            session['user']=username
+            return redirect(url_for('Welcome'))
+        else:
+            return Response('Invalid credentials',401)
+    return '''
+        <form method="POST">
+            Username: <input name="username"><br>
+            Password: <input name="password" type="password"><br>
+            <input type="submit">
+        </form>'''
+@app.route('/welcome')
+def Welcome():
+    if 'user' in session:
+        return f"Welcome {session['user']}"
+    return redirect(url_for('login'))
+if __name__=="__main__":
+    app.run(debug=True)
