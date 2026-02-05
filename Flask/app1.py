5001a5a29b4f7ad2f8ace43f9e4b3dea22891c11
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-02-05 15:23:29 +0530

    more code

---
+++ Flask/app1.py
@@ -0,0 +1,43 @@
+from flask import Flask,redirect,url_for,session,Response,request
+
+app=Flask(__name__)
+app.secret_key="supersecretkey"
+@app.route('/', methods=['POST','GET'])
+def login():
+    if request.method=='POST':
+        username=request.form.get('username')
+        password=request.form.get('password')
+        if username=='admin' and password=='123':
+            session['user']=username
+            return redirect(url_for('Welcome'))
+        else:
+            return Response('Invalid credentials',401)
+    return '''
+        <form method="POST">
+            Username: <input name="username" required><br>
+            Password: <input name="password" type="password" required><br>
+            <input type="submit">
+        </form>'''
+@app.route('/welcome')
+def Welcome():
+    if 'user' in session:
+        return f"Welcome {session['user']}"
+    '''<a href={url_for('logout')}></a>'''
+    return redirect(url_for('login'))
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect(url_for('login'))
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
+# from flask import Flask,render_template
+# app=Flask(__name__)
+# @app.route('/')
+# def login():
+#     return render_template('index.html')
+
+# if __name__=="__main__":
+#     app.run()
--- Flask/land.py
+++ Flask/land.py
@@ -1,8 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask,request,render_template
 app=Flask(__name__)
-@app.route('/')
-def login():
-    return render_template('index.html')
+@app.route('/feedback',methods=['GET','POST'])
+def feedback():
+    error=None
 
+    if request.method=="POST":
+        name=request.form.get('name')
+        email=request.form.get('email')
+        message=request.form.get('message')
+        if not name or not email or not message:
+            error="All fields need to entered"
+            return render_template('feedback.html',error=error)
+        elif '@' not in email:
+            error="Invalid email address"
+            return render_template('feedback.html',error=error)
+        else:
+            print(name,email,message)
+        return render_template('thankyou.html',name=name)
+    return render_template('feedback.html',error=error)
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
