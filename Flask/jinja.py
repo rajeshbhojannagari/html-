55f329ed9e0492bd210734f239f46cd5da96b2cc
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-02-06 11:30:34 +0530

    more code

--- Flask/jinja.py
+++ Flask/jinja.py
@@ -1,9 +1,18 @@
-from flask import Flask,render_template
-app=Flask(__name__)
-@app.route('/')
-def hello():
-    res_code=45000
-    list1=['rajesh','rohith','srikar']
-    return render_template('jinja.html',res_code=res_code,names=list1)
-if __name__=="__main__":
-    app.run()
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
